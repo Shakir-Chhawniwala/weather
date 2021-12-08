22da912caf01aa68c37c115507c9b8f894419cde
Shakir-Chhawniwala <64503320+Shakir-Chhawniwala@users.noreply.github.com>
2021-12-09 00:26:06 +0530

    Serverless Deployment

--- functions/weatherHandler.js
+++ functions/weatherHandler.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
 const axios = require("axios");
 
-const WEATHERAPI = process.env.WEATHERAPI;
+const WEATHER_API = process.env.WEATHERAPI;
 
 exports.handler = async function (event, context, callback) {
   try {
     const { cityTerm } = event.queryStringParameters;
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityTerm}&units=metric&appid=${WEATHERAPI}`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityTerm}&units=metric&appid=${WEATHER_API}`
     );
-    // const data = await response.json();
+
     return {
       statusCode: 200,
       body: JSON.stringify(response.data),
